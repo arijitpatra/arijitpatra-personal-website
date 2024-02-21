dad0d009e922f83e6ca2cd7c50c089d823a660b6
Arijit Patra <ari.clicks@gmail.com>
2024-02-21 19:16:14 +0100

    Initial commit with NextJS configuration
    Initial commit with NextJS configuration

---
+++ app/components/Footer.tsx
@@ -0,0 +1,8 @@
+export function Footer() {
+
+    return (
+        <footer className="flex justify-center mb-12 text-slate-500">
+            <small><i>&quot;There are no strangers here, only friends you haven&apos;t yet met&quot;</i></small>
+        </footer>
+    )
+}
